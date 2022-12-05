10fca750a6ded4823a396cb7277560b800c141ac
BowYard <decgiovanni9191@gmail.com>
2022-12-05 19:08:12 +0100

    [Fix] Matches are now removed from the map [Refactor] New abstract method getLobby()

--- instance/src/main/java/it/bowyard/pixel/match/PixelMatchManager.java
+++ instance/src/main/java/it/bowyard/pixel/match/PixelMatchManager.java
@@ -8,10 +8,7 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.redisson.api.RMapCache;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
 public abstract class PixelMatchManager<E extends Enum<E> & PixelType, T extends SharedMatch<E>, C extends Match<E, T>> {
@@ -33,27 +30,30 @@ public abstract class PixelMatchManager<E extends Enum<E> & PixelType, T extends
     }
 
     public void flush() {
-        for (C value : matches.values()) {
-            removeMatch(value);
+        Iterator<C> iterator = matches.values().iterator();
+        while (iterator.hasNext()) {
+            C match = iterator.next();
+            removeMatch(match);
         }
     }
+
+    public abstract String getLobby();
 
     public void removeMatch(C match) {
         match.getJoining().destroy();
         match.getJoining().delete();
         match.getShared_spectators().destroy();
         match.getShared_spectators().delete();
+        matches.remove(match.getShared().getName());
         shared.remove(match.getShared().getName());
 
         World world = Bukkit.getWorld(match.getWorldName());
 
         CompletableFuture.runAsync(() -> {
-            for (Player player : world.getPlayers()) {
-                Basement.get().getPlayerManager().sendToGameLobby(player.getName(), "bridge_lobby_");
-            }
-        }).whenComplete((voidValue, error) -> {
-            StaticTask.runBukkitTaskTimer(new WorldRemoverTask(world), 0L, 10L, false);
-        });
+            for (Player player : world.getPlayers())
+                Basement.get().getPlayerManager().sendToGameLobby(player.getName(), getLobby());
+        }).whenComplete((voidValue, error) ->
+                StaticTask.runBukkitTaskTimer(new WorldRemoverTask(world), 0L, 10L, false));
     }
 
     public C getMatch(String name) {
