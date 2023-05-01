f8acc8a5b789c96f69e0d46611e54e0f84ab47b1
ohAlee <alebartoh@gmail.com>
2023-05-01 16:13:58 +0200

    Typo

--- common/src/main/java/it/ohalee/pixel/match/SharedMatchStatus.java
+++ common/src/main/java/it/ohalee/pixel/match/SharedMatchStatus.java
@@ -2,7 +2,7 @@ package it.ohalee.pixel.match;
 
 public enum SharedMatchStatus {
     OPEN, // pre lobby
-    WAITING_LAST, // pre lobby double checking last player joining
+    WAITING_LAST, // pre lobby double-checking last player joining
     STARTING, // pre lobby -> starting
     CLOSE, // in-game status
     DIED // match finished
